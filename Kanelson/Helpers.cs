0558c3a834f479610cf26837848a55015324f2af
Yan Pitangui <yanpitangui@gmail.com>
2023-05-25 17:12:34 -0300

    UP: Remover estilo do componente e importar componente de progresso do ant

--- Kanelson/Helpers.cs
+++ Kanelson/Helpers.cs
@@ -26,4 +26,52 @@ public static class Helpers
             .WithAutomaticReconnect()
             .Build();
     }
+    
+    public class ClassMapper
+    {
+        private readonly Dictionary<Func<string>, Func<bool>> _map = new();
+
+        public ClassMapper() : this(string.Empty)
+        {
+        }
+
+        public ClassMapper(string originalClass)
+        {
+            OriginalClass = originalClass;
+
+            _map.Add(() => OriginalClass, () => !string.IsNullOrEmpty(OriginalClass));
+        }
+
+        public string Class => ToString();
+
+        public string OriginalClass { get; internal set; }
+        
+
+        public override string ToString() => string.Join(' ', _map.Where(i => i.Value()).Select(i => i.Key()));
+
+        public ClassMapper Add(string name) => Get(() => name);
+
+        public ClassMapper Get(Func<string> funcName)
+        {
+            _map.Add(funcName, () => !string.IsNullOrEmpty(funcName()));
+            return this;
+        }
+
+        public ClassMapper GetIf(Func<string> funcName, Func<bool> func)
+        {
+            _map.Add(funcName, func);
+            return this;
+        }
+
+        public ClassMapper If(string name, Func<bool> func) => GetIf(() => name, func);
+
+        public ClassMapper Clear()
+        {
+            _map.Clear();
+
+            _map.Add(() => OriginalClass, () => !string.IsNullOrEmpty(OriginalClass));
+
+            return this;
+        }
+    }
 }

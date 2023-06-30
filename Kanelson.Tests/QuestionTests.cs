5086531864d7560257d316e7b13df8d53a9545e2
Yan Pitangui <yanpitangui@gmail.com>
2023-06-30 19:14:59 -0300

    UP: Preparação para testes unitários

---
+++ Kanelson.Tests/QuestionTests.cs
@@ -0,0 +1,9 @@
+using Akka.Persistence.TestKit;
+using Akka.TestKit.Xunit2;
+
+namespace Kanelson.Tests;
+
+public class QuestionTests : PersistenceTestKit
+{
+    
+}
--- Kanelson.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Kanelson.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
